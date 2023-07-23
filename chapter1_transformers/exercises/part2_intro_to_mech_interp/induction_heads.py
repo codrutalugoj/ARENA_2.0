ed0746f3ac7d2fd018e1b702a7d99ab958ba027b
codrutalugoj <codrutalugoj@gmail.com>
2023-07-23 11:00:57 +0100

    final induction heads

--- chapter1_transformers/exercises/part2_intro_to_mech_interp/induction_heads.py
+++ chapter1_transformers/exercises/part2_intro_to_mech_interp/induction_heads.py
@@ -145,3 +145,87 @@ def first_attn_detector(cache: ActivationCache, threshold: float = 0.7) -> List[
 print("Heads attending to current token  = ", ", ".join(current_attn_detector(cache)))
 print("Heads attending to previous token = ", ", ".join(prev_attn_detector(cache)))
 print("Heads attending to first token    = ", ", ".join(first_attn_detector(cache)))
+
+
+# %%
+def generate_repeated_tokens(
+    model: HookedTransformer, seq_len: int, batch: int = 1
+) -> Int[Tensor, "batch full_seq_len"]:
+    '''
+    Generates a sequence of repeated random tokens
+
+    Outputs are:
+        rep_tokens: [batch, 1+2*seq_len]
+    '''
+    prefix = (t.ones(batch, 1) * model.tokenizer.bos_token_id).long()
+
+    token_idx = t.randint(low=0, high=cfg.d_vocab, size=(batch, seq_len), dtype=t.int64)
+    rep_tokens = t.cat([prefix, token_idx, token_idx], dim=-1).to(device)
+
+    return rep_tokens
+
+def run_and_cache_model_repeated_tokens(model: HookedTransformer, seq_len: int, batch: int = 1) -> Tuple[t.Tensor, t.Tensor, ActivationCache]:
+    '''
+    Generates a sequence of repeated random tokens, and runs the model on it, returning logits, tokens and cache
+
+    Should use the `generate_repeated_tokens` function above
+
+    Outputs are:
+        rep_tokens: [batch, 1+2*seq_len]
+        rep_logits: [batch, 1+2*seq_len, d_vocab]
+        rep_cache: The cache of the model run on rep_tokens
+    '''
+    rep_tokens = generate_repeated_tokens(model, seq_len, batch)
+    rep_logits, rep_cache = model.run_with_cache(rep_tokens)
+
+    return rep_tokens, rep_logits, rep_cache
+
+
+seq_len = 50
+batch = 1
+(rep_tokens, rep_logits, rep_cache) = run_and_cache_model_repeated_tokens(model, seq_len, batch)
+rep_cache.remove_batch_dim()
+rep_str = model.to_str_tokens(rep_tokens)
+model.reset_hooks()
+log_probs = get_log_probs(rep_logits, rep_tokens).squeeze()
+
+print(f"Performance on the first half: {log_probs[:seq_len].mean():.3f}")
+print(f"Performance on the second half: {log_probs[seq_len:].mean():.3f}")
+
+plot_loss_difference(log_probs, rep_str, seq_len)
+
+# %%
+
+print(rep_tokens)
+print()
+
+for l in range(cfg.n_layers):
+    print("Layer ", l)
+    display(cv.attention.attention_patterns(
+        tokens=rep_str, 
+        attention=rep_cache["pattern", l],
+        attention_head_names=[f"L{l}H{i}" for i in range(cfg.n_heads)],
+    ))
+
+
+# %%
+def induction_attn_detector(cache: ActivationCache, threshold: float = 0.4) -> List[str]:
+    '''
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be induction heads
+
+    Remember - the tokens used to generate rep_cache are (bos_token, *rand_tokens, *rand_tokens)
+    '''
+    out = [] 
+    n_induction_tokens = (cache["pattern", 0][0].shape[0])//2
+
+    for l in range(cfg.n_layers):
+        for head_num in range(cfg.n_heads): 
+            token_attended = cache["pattern", l][head_num].diagonal(offset=-n_induction_tokens+1) 
+            score = token_attended.mean()
+            print(score)
+            if score > threshold:
+                out.append(f"L{l}H{head_num}")
+
+    return out
+
+print("Induction heads = ", ", ".join(induction_attn_detector(rep_cache)))
